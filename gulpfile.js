e07e02acbb99018c5ffb36a5d31e21f6d24a06a0
Miguel Angel Herrera <ambientmiguel@gmail.com>
2022-04-29 15:41:27 -0500

    iniciando con codigo

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+const { src, dest, watch, series, parallel } = require("gulp");
+
+// CSS y SASS
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+// Imagenes
+
+const imagemin = require( "gulp-imagemin");
+
+function css( done ) {
+    // compilar
+    // pasos: 1- identificar archivo, 2- compilarla, 3- guardar el css
+
+    src("src/scss/app.scss")
+        .pipe( sass() )
+        .pipe( postcss( [autoprefixer() ] ) )
+        .pipe( dest("build/css") )
+
+
+    done();
+}
+
+function imagenes ( done ) {
+    src("src/img/**/*")
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest("build/img") );
+
+    done();
+}
+
+function dev() {
+    
+    watch("src/scss/**/*.scss", css);
+    watch("src/img/**/*", imagenes);
+    
+}
+
+
+
+exports.css = css;
+exports.dev = dev; 
+exports.imagenes = imagenes;
+exports.default = series( imagenes, css, dev ) ;
+
+// series - se inicia un tarea ya hasta que finalza sigue con la siguiente
+
+// parallel - todas las tareas inician al mismo tiempo
